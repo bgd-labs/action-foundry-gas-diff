d58e19561a93b96d031f4011ee512ef5640c642f
Jonas Strassel <info@jonas-strassel.de>
2025-01-29 19:44:01 +0100

    feat: replace with `snapshot/*.json` diffing (#5)
    * feat: add snapshot diffing
    
    * fix: change snappy a bit
    
    * experiment: log ctx
    
    * fix: use ref from context
    
    * docs: update readme, readd ref

--- src/action.ts
+++ src/action.ts
@@ -1,25 +1,69 @@
-import { readFileSync, existsSync } from "fs";
-import { getInput, setOutput } from "@actions/core";
-import { context } from "@actions/github";
-import { getHtmlGasReport } from "./lib";
+import { readFile } from "node:fs/promises";
+import { debug, getInput, setOutput } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+import { create as createGlob } from "@actions/glob";
+import { formatDiffMd, snapshotDiff } from "./lib";
+import path from "node:path";
 
-const root = getInput("ROOT_REPORT_PATH");
-const current = getInput("REPORT_PATH");
+const heading = getInput("heading");
+const octokit = getOctokit(getInput("token"));
 
-const rootExists = root && existsSync(root);
-const currentExists = current && existsSync(current);
+const getBaseFile = async (path: string, ref: string) => {
+  const { data } = await octokit.rest.repos.getContent({
+    repo: context.repo.repo,
+    owner: context.repo.owner,
+    path,
+    ref,
+  }).catch(e => {
+    return e.response as {
+      status: number
+      data: {
+        message: string;
+        status: number;
+      };
+    };
+  })
 
-if (!currentExists) throw new Error("gas report not found");
+  if (!data || !("content" in data)) {
+    return {};
+  }
 
-const rootContent = rootExists ? JSON.parse(readFileSync(root, "utf8")) : [];
-const currentContent = JSON.parse(readFileSync(current, "utf8"));
+  return JSON.parse(atob(data.content));
+};
 
-const table = getHtmlGasReport(rootContent, currentContent, {
-  rootUrl: `${context.payload.repository?.html_url}/blob/${context.sha}/`,
-  ignoreUnchanged: getInput("ignoreUnchanged") === "true",
-});
 
-setOutput(
-  "report",
-  `<details><summary>:fuelpump: <strong>Gas report</strong></summary>\n\n${table}\n\n</details>`,
-);
+const run = async () => {
+  debug("Starting run");
+  const base_ref = getInput("base_ref") === "" ? undefined : getInput("base_ref");
+  const ref = context.payload.pull_request?.base.ref;
+  if (!base_ref && !ref) {
+    throw new Error("No 'base_ref' provided and not a pull request");
+  }
+  const results: Parameters<typeof formatDiffMd>[1] = [];
+  const globber = await createGlob(getInput("files"));
+  const files = await globber.glob();
+  debug(`Files to compare: ${files.join(", ")}`);
+
+  for (const filePath of files) {
+    const relativePath = path.relative(process.cwd(), filePath);
+    debug(`Comparing ${relativePath}`);
+    const before = await getBaseFile(relativePath, base_ref ?? ref);
+    const after = JSON.parse(await readFile(filePath, "utf8"));
+    const diff = snapshotDiff({
+      before,
+      after,
+    });
+    results.push({
+      path: relativePath,
+      diff,
+    });
+  }
+
+  debug(`Results: ${JSON.stringify(results)}`);
+  const report = formatDiffMd(heading, results);
+  debug(`Report: ${report}`);
+
+  setOutput("report", report);
+}
+
+run()
--- src/lib.spec.ts
+++ src/lib.spec.ts
@@ -1,29 +1,107 @@
 import { expect, it, describe } from "vitest";
-import { getHtmlGasReport } from "./lib";
-import gas from "../mocks/gas.json";
-import gasRoot from "../mocks/gas.backup.json";
+import { formatDiffMd, snapshotDiff } from "./lib";
+
+const before = {
+  "test_removed": "123",
+  "test_unchanged": "456",
+  "test_bigger": "789",
+  "test_smaller": "789",
+}
+
+const after = {
+  "test_unchanged": "456",
+  "test_bigger": "1111",
+  "test_smaller": "654",
+  "test_added": "789",
+}
 
 describe("lib", () => {
-  it("should generate a well formatted report with empty root", () => {
+  it("should throw when not passed numbers in before", () => {
+    expect(() => snapshotDiff({
+      before: {
+        "test": "abc",
+      },
+      after: {
+        "test": "123",
+      },
+    })).toThrowErrorMatchingInlineSnapshot(`[Error: The following keys in before are not numbers: test]`);
+  })
+
+  it("should throw when not passed numbers in after", () => {
+    expect(() => snapshotDiff({
+      before: {
+        "test": "123",
+      },
+      after: {
+        "test": "abc",
+      },
+    })).toThrowErrorMatchingInlineSnapshot(`[Error: The following keys in after are not numbers: test]`);
+  })
+
+  it("should detect differences between two different snapshots", () => {
+
+
     expect(
-      getHtmlGasReport([], gas as any, {
-        rootUrl: "https://github.com/",
+      snapshotDiff({
+        before,
+        after
       }),
-    ).toMatchSnapshot();
+    ).toMatchInlineSnapshot(`
+      {
+        "added": {
+          "test_added": "789",
+        },
+        "changed": {
+          "test_bigger": "<sup>↑41% (+322)</sup> 1,111",
+          "test_smaller": "<sup>↓17% (-135)</sup> 654",
+        },
+        "removed": {
+          "test_removed": "123",
+        },
+        "unchanged": {
+          "test_unchanged": "456",
+        },
+      }
+    `);
   });
-  it("should generate a well formatted report with existing root", () => {
-    expect(
-      getHtmlGasReport(gasRoot as any, gas as any, {
-        rootUrl: "https://github.com/",
-      }),
-    ).toMatchSnapshot();
-  });
-  it("should generate a well formatted report with existing root & skip unchanged", () => {
-    expect(
-      getHtmlGasReport(gasRoot as any, gas as any, {
-        rootUrl: "https://github.com/",
-        ignoreUnchanged: true,
-      }),
-    ).toMatchSnapshot();
-  });
+
+  it("nicely formats the diff as markdown", () => {
+    const diffA = snapshotDiff({
+      before,
+      after
+    })
+
+    const diffB = snapshotDiff({
+      before,
+      after
+    })
+
+    const result = formatDiffMd("Abcdef", [{ path: "path_a", diff: diffA }, { path: "path_b", diff: diffB }])
+    console.log(result)
+    expect(result).toMatchInlineSnapshot(`
+      "### ♻️ Abcdef
+      | Path | Value |
+      | --- | ---: |
+      | **path_a** |  |
+      | test_bigger | <sup>↑41% (+322)</sup> 1,111 |
+      | test_smaller | <sup>↓17% (-135)</sup> 654 |
+      | test_removed | 123 |
+      | test_added | 789 |
+      | **path_b** |  |
+      | test_bigger | <sup>↑41% (+322)</sup> 1,111 |
+      | test_smaller | <sup>↓17% (-135)</sup> 654 |
+      | test_removed | 123 |
+      | test_added | 789 |
+
+      <details><summary>🔕 Unchanged</summary>
+
+      | Path | Value |
+      | --- | ---: |
+      | **path_a** |  |
+      | test_unchanged | 456 |
+      | **path_b** |  |
+      | test_unchanged | 456 |
+      </details>"
+    `)
+  })
 });
